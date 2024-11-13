9bd8b92ee2786c4734f29c4b5b422933b046d096
Lemelo <lecarvalh@gmail.com>
2024-11-13 12:31:51 -0300

    Update first.py

--- first.py
+++ first.py
@@ -11,3 +11,29 @@ numero = st.slider('Selecione um número', min_value=0, max_value=100)
 st.text('Seu número é ' + str(numero))
 resposta = st.slider('Informe o grau de satisfação:', min_value=0, max_value=100)
 st.write(f'A resposta do usuario foi {str(resposta)}')
+# criando elementos gráficos
+'''Informar como colher os dados através de variáveis'''
+x = st.checkbox('Sim')
+st.write(x)
+
+# fazer o mesmo com alguns dos comandos abaixo
+st.title(x)
+st.button('Clique')
+st.radio('Selecione seu gênero',['Masculino','Feminino'])
+st.selectbox('Selecione seu gênero',['Masculino','Feminino'])
+st.multiselect('Escolha um departamento',['DCS', 'DE', 'DIR'])
+st.select_slider('Selecione uma resposta', ['Ruim', 'Bom', 'Excelente'])
+st.slider('Selecione um número', 0,50)
+st.number_input('Selecione um número', 0,10)
+st.text_input('Endereço de e-mail')
+st.date_input('Data de viagem')
+st.time_input('Tempo de escola')
+st.text_area('Descrição')
+st.file_uploader('Atualize uma foto')
+st.color_picker('Escolha sua cor favorita')
+
+# mensagens de status
+st.success("Você conseguiu!")
+st.error("Erro!")
+st.warning("Advertência")
+st.info("Esta é uma informação")
