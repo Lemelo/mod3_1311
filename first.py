a2e98fa2113692fc12265c6133d61dd8966c63c5
Lemelo <lecarvalh@gmail.com>
2024-11-13 12:36:00 -0300

    Update first.py

--- first.py
+++ first.py
@@ -11,12 +11,8 @@ numero = st.slider('Selecione um número', min_value=0, max_value=100)
 st.text('Seu número é ' + str(numero))
 resposta = st.slider('Informe o grau de satisfação:', min_value=0, max_value=100)
 st.write(f'A resposta do usuario foi {str(resposta)}')
-# criando elementos gráficos
-'''Informar como colher os dados através de variáveis'''
 x = st.checkbox('Sim')
 st.write(x)
-
-# fazer o mesmo com alguns dos comandos abaixo
 st.title(x)
 st.button('Clique')
 st.radio('Selecione seu gênero',['Masculino','Feminino'])
@@ -31,8 +27,6 @@ st.time_input('Tempo de escola')
 st.text_area('Descrição')
 st.file_uploader('Atualize uma foto')
 st.color_picker('Escolha sua cor favorita')
-
-# mensagens de status
 st.success("Você conseguiu!")
 st.error("Erro!")
 st.warning("Advertência")
